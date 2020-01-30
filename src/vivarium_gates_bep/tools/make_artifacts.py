a04e40271e7149df2a486e1765ec2d5faa3a3813
James Collins <collijk@uw.edu>
2020-01-30 09:05:49 -0800

    Layout structure for cli

---
+++ src/vivarium_gates_bep/tools/make_artifacts.py
@@ -0,0 +1,13 @@
+"""Main application functions for building artifacts.
+
+.. admonition::
+
+   Logging in this module should typically be done at the ``info`` level.
+   Use your best judgement.
+
+"""
+
+
+def build_artifacts(location: str, output_dir: str, append: bool, verbose: int):
+    pass
+
---
+++ src/vivarium_gates_bep/tools/make_specs.py
@@ -0,0 +1,4 @@
+"""Main application functions for producing model specifications."""
+
+def build_model_specifications(template: str, location: str, output_dir: str):
+    pass
