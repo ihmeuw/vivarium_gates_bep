c8e936dc2a7560940bf1b4a06cf6d9789f8a9ec4
James Collins <collijk@uw.edu>
2020-02-03 02:46:40 -0800

    Pull in and clean up lbwsg

--- src/vivarium_gates_bep/components/__init__.py
+++ src/vivarium_gates_bep/components/__init__.py
@@ -1,2 +1,3 @@
 from .population import NewbornPopulation
 from .observers import DiseaseObserver, NeonatalDisordersObserver, ChildGrowthFailureObserver
+from .lbwsg import LBWSGRisk, LBWSGRiskEffect
