0c7ccf41b2f482296bfde89d64e225614823e121
James Collins <collijk@uw.edu>
2020-02-04 20:22:47 -0800

    Add function to sample from a scaled beta distribution

--- src/vivarium_gates_bep/utilites.py
+++ src/vivarium_gates_bep/utilites.py
@@ -1,3 +1,5 @@
+import numpy as np
+import scipy.stats
 
 
 def sanitize_location(location: str):
@@ -16,3 +18,35 @@ def sanitize_location(location: str):
     """
     # FIXME: Should make this a reversible transformation.
     return location.replace(" ", "_").replace("'", "_").lower()
+
+
+def sample_beta_distribution(seed: int, mean: float, variance: float, upper_bound: float, lower_bound: float) -> float:
+    """Gets a single random draw from a scaled beta distribution.
+
+    Parameters
+    ----------
+    seed
+        Seed for the random number generator.
+    mean
+        The mean of the scaled beta distribution.
+    variance
+        The variance of the scaled beta distribution.
+    upper_bound
+        The upper bound of the support of the scaled beta distribution.
+    lower_bound
+        The lower bound of the support of the scaled beta distribution.
+
+    Returns
+    -------
+        The random variate from the scaled beta distribution.
+
+    """
+    np.random.seed(seed)
+    support_width = (upper_bound - lower_bound)
+    mean = (mean - lower_bound) / support_width
+    variance /= support_width ** 2
+    alpha = mean * (mean * (1 - mean) / variance - 1)
+    beta = (1 - mean) * (mean * (1 - mean) / variance - 1)
+    return lower_bound + support_width*scipy.stats.beta.rvs(alpha, beta)
+
+
