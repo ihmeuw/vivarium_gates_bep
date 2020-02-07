1478e73b723bb0fa81aba71b2a0bb76e858eb0fa
James Collins <collijk@uw.edu>
2020-02-07 04:58:56 -0800

    compute and write final data

--- src/vivarium_gates_bep/tools/make_results.py
+++ src/vivarium_gates_bep/tools/make_results.py
@@ -9,9 +9,11 @@ from vivarium_gates_bep.results_processing import process_results
 def build_results(output_file: str):
     output_file = Path(output_file)
     measure_dir = output_file.parent / 'count_data'
-    if measure_dir.exists():
-        shutil.rmtree(measure_dir)
-    measure_dir.mkdir(exist_ok=True, mode=0o775)
+    results_dir = output_file.parent / 'final_data'
+    for d in [measure_dir, results_dir]:
+        if d.exists():
+            shutil.rmtree(d)
+        d.mkdir(exist_ok=True, mode=0o775)
 
     logger.info(f'Reading in output data from {str(output_file)}.')
     data, keyspace = process_results.read_data(output_file)
@@ -25,3 +27,8 @@ def build_results(output_file: str):
     measure_data = process_results.make_measure_data(data)
     logger.info(f'Writing raw count and proportion data to {str(measure_dir)}')
     measure_data.dump(measure_dir)
+    logger.info(f'Computing final_data.')
+    final_data = process_results.make_final_data(measure_data)
+    logger.info(f'Writing final data to {str(results_dir)}')
+    final_data.dump(results_dir)
+    logger.info('**DONE**')
