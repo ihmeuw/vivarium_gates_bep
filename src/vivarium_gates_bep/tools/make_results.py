1e93ae8a50acc2b0ed01222a55c2754dcad38f0c
James Collins <collijk@uw.edu>
2020-02-06 22:16:05 -0800

    remove existing dirs, set permissions

--- src/vivarium_gates_bep/tools/make_results.py
+++ src/vivarium_gates_bep/tools/make_results.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import shutil
 
 from loguru import logger
 
@@ -13,5 +14,7 @@ def build_results(output_file: str):
     logger.info(f'Computing count space data.')
     measure_data = process_results.make_measure_data(data)
     measure_dir = output_file.parent / 'count_data'
-    measure_dir.mkdir(exist_ok=True)
+    if measure_dir.exists():
+        shutil.rmtree(measure_dir)
+    measure_dir.mkdir(exist_ok=True, mode=0o775)
     measure_data.dump(measure_dir)
