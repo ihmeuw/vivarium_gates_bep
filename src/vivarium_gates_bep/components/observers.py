97e6a7b3f38ba88e2b167abc4310a7a13217b1ce
James Collins <collijk@uw.edu>
2020-02-21 22:50:11 -0800

    Yuck, left a debugger

--- src/vivarium_gates_bep/components/observers.py
+++ src/vivarium_gates_bep/components/observers.py
@@ -278,7 +278,6 @@ class ChildGrowthFailureObserver():
             stats[f'wasting_z_score_sd_at_six_months'] = pop.wasting_z.std()
             stats[f'stunting_z_score_mean_at_six_months'] = pop.wasting_z.mean()
             stats[f'stunting_z_score_sd_at_six_months'] = pop.wasting_z.std()
-            import pdb; pdb.set_trace()
             for cat, value in dict(pop.wasting_cat.value_counts()).items():
                 stats[f'wasting_{cat}_exposed_at_six_months'] = value
             for cat, value in dict(pop.stunting_cat.value_counts()).items():
